3b12ac1f109aa9707db46b0c728df166c49032df
Carlos123432 <80365760+Carlos123432@users.noreply.github.com>
2025-08-03 15:53:48 -0700

    Add files via upload

--- module4.py
+++ module4.py
@@ -1,1 +1,14 @@
+# Carlos Mendez, module4.py
 
+N = int(input("Positive Integer: "))
+numbers = []
+for i in range(N):
+    num = int(input("Enter number {i + 1}: "))
+    numbers.append(num)
+
+X = int(input("Enter the number to search for (X): "))
+
+if X in numbers:
+    print(numbers.index(X) + 1)
+else:
+    print(-1)
